c89936c9e296daada19a17890e59546d3f2b2fe3
Abyaz <58655276+MuhammadAbyaz@users.noreply.github.com>
2024-04-28 21:51:09 +0500

    feat: add user and bot images and more styling

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const responseClassList = ["text-black", "border", "rounded-xl", "p-2", "m-2"];
-
+const responseClassList = [
+  "text-black",
+  "p-2",
+  "m-2",
+  "flex",
+  "items-center",
+  "gap-x-3",
+];
+const questionClassList = [
+  "text-black",
+  "m-2",
+  "p-2",
+  "flex",
+  "items-center",
+  "gap-x-3",
+];
 const loaderDivClass = [
   "inline-block",
   "h-8",
@@ -29,8 +43,29 @@ const prompt = document.querySelector("#prompt");
 const generateResponse = document.querySelector("#generate-response");
 const reponseBody = document.querySelector("#responses");
 const loader = document.createElement("div");
-
+loaderDivClass.map((value) => {
+  loader.classList.add(value);
+});
+const loaderSpan = document.createElement("span");
+loaderSpanClass.map((value) => loaderSpan.classList.add(value));
+loaderSpan.innerText = "Loading...";
+loader.append(loaderSpan);
+loader.role = "status";
 function getResponse(prompt, output) {
+  generateResponse.classList.add("cursor-not-allowed");
+  generateResponse.classList.add("opacity-50");
+  const robotImage = document.createElement("img");
+  robotImage.src = "/robot.png";
+  robotImage.alt = "robot";
+  robotImage.classList.add("h-10");
+  robotImage.classList.add("w-10");
+  const imageDiv = document.createElement("div");
+  imageDiv.classList.add("flex");
+  imageDiv.classList.add("flex-col");
+  imageDiv.classList.add("justify-start");
+  imageDiv.classList.add("h-full");
+  const text = document.createElement("div");
+  text.classList.add("max-w-[90%]");
   const data = {
     model: "llama3",
     prompt: prompt,
@@ -47,36 +82,49 @@ function getResponse(prompt, output) {
     const decoder = new TextDecoder();
     const readData = async () => {
       const { value, done } = await reader.read();
-      if (done) return "Stream Ended";
-      if (
-        output.innerText === "Generating Response\nLoading..." &&
-        output.innerHTML
-      ) {
-        output.innerText = "";
-        output.innerHTML = "";
+      if (done) {
+        generateResponse.classList.remove("cursor-not-allowed");
+        generateResponse.classList.remove("opacity-50");
+        return "Stream Ended";
       }
-      output.innerText += JSON.parse(decoder.decode(value)).response;
+      if (output.innerText === "Generating Response\nLoading...") {
+        output.innerText = "";
+        imageDiv.append(robotImage);
+        output.append(imageDiv);
+        output.append(text);
+      }
+      text.innerText += JSON.parse(decoder.decode(value)).response;
       readData();
     };
     readData();
   });
 }
 
-loaderDivClass.map((value) => {
-  loader.classList.add(value);
-});
-const loaderSpan = document.createElement("span");
-loaderSpanClass.map((value) => loaderSpan.classList.add(value));
-loaderSpan.innerText = "Loading...";
-loader.append(loaderSpan);
-loader.role = "status";
 generateResponse.addEventListener("click", () => {
+  const imageDiv = document.createElement("div");
+  imageDiv.classList.add("flex");
+  imageDiv.classList.add("flex-col");
+  imageDiv.classList.add("justify-start");
+  imageDiv.classList.add("h-full");
+  const userImage = document.createElement("img");
+  userImage.src = "/man.png";
+  userImage.alt = "man";
+  userImage.classList.add("h-10");
+  userImage.classList.add("w-10");
+  imageDiv.append(userImage);
+  const questionDiv = document.createElement("div");
+  questionClassList.map((val) => questionDiv.classList.add(val));
   const value = prompt.value;
+  questionDiv.appendChild(imageDiv);
+  const text = document.createElement("div");
+  text.innerText = value;
+  questionDiv.append(text);
   prompt.value = "";
   const output = document.createElement("div");
   responseClassList.map((value) => output.classList.add(value));
   output.innerText = "Generating Response";
   output.append(loader);
+  reponseBody.append(questionDiv);
   reponseBody.append(output);
   getResponse(value, output);
 });
